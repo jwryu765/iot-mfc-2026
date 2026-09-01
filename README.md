a8bd03c8b9c8b34c139070f7978b9e1c1df1a9fb
jwryu765 <jhryu7658@gmail.com>
2026-09-01 12:32:53 +0900

    MFC 1일차

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # iot-mfc-2026
+
 IoT반 MS MFC 학습 리포지토리
+
+
+## MFC?
+
+Microsoft Foundation Class 의 약자. C++ 기반의 윈도우 프로그래밍 라이브러리 중 하나
+
+GUI를 디자인, 실행하는 앱을 만들기 위해서 MS에서 만든 기술
+
+- Linux : Qt, GTK, wxWidgets ....
+- Windows : Qt, Win32API, ...
+
+리눅스와 윈도우가 OS기반이 다르기때문에 초기에 표준화를 못함. Win32API 기반으로 GUI를 라이브러리화 -> MFC
+
+윈도우에서만 동작하는 GUI 라이브러리 프레임워크임.
+
+현재는 리룩스에서 Wine 에뮬레이터로 Windows 기반 GUI도 실행 가능(PC카톡 윈도우 버전)
+
+
+### MFC 단점 및 장점
+
+#### 단점
+
+- 너무 어렵다. 최소 6개월은 공부를 해야하는 문제
+- 오래된 기술로 생각보다 최신 튜토리얼이 별로 없음.
+- 기업이 예전부터 사용하던 솔루션 기반이 변경하기 힘들어서 계속 사용
+
+#### 장점
+
+- C++ 자체가 어려운데 MFC를 사용하면 복잡한 Win32 API를 직접 다루지 않아도 구현이 가능
+- 생산성 향상 : 기존 C/C++ 대비하여 생산성 높다는 의미. C#, Java등 OOP와는 비교하면 안됨
+- C++ 실력이 많이 좋아짐
+
+
+### MFC 학습 링크
+
+- Microsoft Learn :
+
+
+
+## MFC 튜토리얼
+
+### 순수 Win32 API로 윈도우 만들기
+
+- 중요! Windows 32 bit에서 C++에 있는 여러 기능을 손쉽게 쓰도록 만들어높은 API(Application Programming Interface)
+- 최초에는 이를 통해서 윈도우를 직접 개발
+
+
+#### Visual Studio 실행
---
+++ basic/MfcBasicSolution/Win32ControlsApp/main.cpp
@@ -0,0 +1,121 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(
+    HWND hwnd,
+    UINT message,
+    WPARAM wParam,
+    LPARAM lParam
+);
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow
+) {
+
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"MyWindowClass";
+    wc.hCursor = LoadCursor(NULL, IDC_HAND);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 20);
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindow(
+        L"MyWindowClass",
+        L"두번째 윈도우",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        800,
+        480,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    MSG msg = {};
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(
+    HWND hwnd,
+    UINT message,
+    WPARAM wParam,
+    LPARAM lParam
+) {
+    switch (message) {
+    case WM_CREATE:  // 윈도우가 생성되면
+
+        // 1. 버튼도 CreateWindow() 함수 사용 - 버튼도 사실 하나의 Window임
+        CreateWindow(
+            L"BUTTON",
+            L"확인",
+            WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+            240,
+            150,
+            100,
+            40,
+            hwnd,
+            (HMENU)1001,
+            NULL,
+            NULL
+        );
+        return 0;
+
+        // 2. 텍스트박스 추가
+        CreateWindow(
+            L"STATIC",
+            L"Hello, Win32API!",
+            WS_CHILD | WS_VISIBLE,
+            210,
+            80,
+            200,
+            30,
+            hwnd,
+            NULL,
+            NULL,
+            NULL
+        );
+        return 0;
+
+    case WM_COMMAND:
+		if (LOWORD(wParam) == 1001) {
+			MessageBox(
+                hwnd,
+                L"버튼이 클릭되었습니다.",
+                L"알림",
+                MB_OKCANCEL
+            );
+		}
+		return 0;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(
+        hwnd,
+        message,
+        wParam,
+        lParam
+    );
+}
